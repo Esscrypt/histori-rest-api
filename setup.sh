ef7a474c68a1115dadb4e32fdea40b2dc16b98d2
Mihail Kirov <me@mikirov.xyz>
2024-10-09 00:13:13 +0300

    WIP

--- setup.sh
+++ setup.sh
@@ -6,19 +6,12 @@ set -e
 # Define the application directory as the current directory
 APP_DIR=$(pwd)
 
-# Move to the application directory (optional since APP_DIR is already current directory)
-cd "$APP_DIR"
-
 # Install dependencies and build the project
 echo "Installing dependencies..."
 npm install
 
 echo "Building the project..."
 npm run build
-
-# Create the .env file (use the SCP'd .env.instance file)
-echo "Moving the .env.instance to .env.production"
-mv /home/ubuntu/.env.instance $APP_DIR/.env
 
 # Create a systemd service file for the application
 SERVICE_NAME="histori-rest-api"
@@ -34,9 +27,8 @@ After=network.target
 [Service]
 ExecStart=/usr/bin/node $APP_DIR/dist/main.js
 WorkingDirectory=$APP_DIR
-EnvironmentFile=$APP_DIR/.env
 Restart=always
-RestartSec=10
+RestartSec=0
 User=nobody
 Group=nogroup
 
